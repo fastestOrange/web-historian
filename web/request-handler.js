4c86934fd685ae2c19eb84bf3fba684fa6d29196
Hack Reactor Students <coliver303@gmail.com>
2014-03-19 15:30:41 -0700

    returns index for blank get request

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,21 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var  httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if(req.method === "GET") {
+    res.writeHead(200, httpHelpers.headers);
+    fs.readFile(path.join(archive.paths.siteAssets, "/index.html"), function (err, data) {
+      if(err) {
+        throw err;
+      }
+      res.end(data);
+      console.log(data);
+    });
+    // res.end();
+
+  }
+  // res.end(archive.paths.list);
 };
