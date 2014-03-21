2ca0ed810f7e67e9473c18145dc7a05173956d7e
Hack Reactor Students <coliver303@gmail.com>
2014-03-20 19:42:59 -0700

    GET and limited POST

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,32 +3,46 @@ var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
 var  httpHelpers = require('./http-helpers');
 var workers = require('../workers/htmlfetcher');
+var _ = require('underscore');
 // require more modules/folders here!
 
 var requestMethods = {
   "GET": function(req, res){
-    archive.readListOfUrls(function(list) {
-      if(!archive.isUrlInList(req.url.slice(1), list)) {
-        res.writeHead(404, httpHelpers.headers);
-        res.end();
-      }else{
-        var filePath = req.url === '/' ? path.join(archive.paths.siteAssets, "/index.html") : path.join(archive.paths.archivedSites, req.url.slice(1));
-        res.writeHead(200, httpHelpers.headers);
-        archive.isURLArchived(req.url.slice(1), function(htmlReady){
-          if(htmlReady){
-            fs.readFile(filePath, function (err, data) {
-              if(err) {
-                throw err;
-              }
-              res.end(data);
-            });
-          }else {
-            //redirect to loading page
-            httpHelpers.serveLoadingPage(res);
-          }
-        });
-      }
-    });
+    var url = req.url.slice(1);
+    if(url === "" || url === "styles.css") {          //check if local resource
+      res.writeHead(200, httpHelpers.headers);
+      var filePath = req.url === '/' ? path.join(archive.paths.siteAssets, "/index.html") : path.join(archive.paths.siteAssets, "/styles.css")
+      fs.readFile(filePath, function (err, data) {
+        if(err) {
+          throw err;
+        }
+        res.end(data);
+      });
+    }else {                                           //external url
+      console.log(url);
+      archive.readListOfUrls(function(list) {
+        if(!archive.isUrlInList(req.url.slice(1), list)) {
+          res.writeHead(404, httpHelpers.headers);
+          res.end();
+        }else{
+          var filePath = path.join(archive.paths.archivedSites, req.url.slice(1));
+          res.writeHead(200, httpHelpers.headers);
+          archive.isURLArchived(req.url.slice(1), function(htmlReady){
+            if(htmlReady){
+              fs.readFile(filePath, function (err, data) {
+                if(err) {
+                  throw err;
+                }
+                res.end(data);
+              });
+            }else {
+              //redirect to loading page
+              httpHelpers.serveLoadingPage(res);
+            }
+          });
+        }
+      });
+    }
   },
 
   "POST": function(req, res){
@@ -47,7 +61,8 @@ var requestMethods = {
           archive.isURLArchived(data, function(htmlReady) { //and in archive
             if(htmlReady) {                                 //and scraped
               var filePath = path.join(archive.paths.archivedSites, data);
-              res.writeHead(302, httpHelpers.headers);      //redirect and serve
+              // res.writeHead(302, _.extend(httpHelpers.headers, {"Location": filePath}));
+              // res.end();      //redirect and serve
 
               fs.readFile(filePath, function (err, data) {
                 if(err) {
